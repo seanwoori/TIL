f606a8b6360599872beef88a209d7e115ee9a63d
seanwoori <seanwoory@gmail.com>
2023-01-18 17:49:10 +0900

    2_2.py 코드 완성 및 업로드

--- handouts/230118/solving_club/2_2.py
+++ handouts/230118/solving_club/2_2.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 T = 10
-for test_case in range(1, T + 1):
+for test_case in range(1, T+1):
     # ///////////////////////////////////////////////////////////////////////////////////
     test_num = input()
     org_mat = []
@@ -11,24 +11,7 @@ for test_case in range(1, T + 1):
     for i in range(n):
         org_lst = list(map(int, input().split()))
         org_mat.append(org_lst)
-    print(org_lst)
-    
-    
 
-    
-    '''    
-    for i in range(n):
-        temp_lst = []
-        count = 0
-        while True:
-            temp_lst.append(org_lst[0])
-            count += 1
-            del org_lst[0]
-            if count == n:
-                break
-        org_mat.append(temp_lst)
-    '''
-    
     row_max = []
     
     raw_col_max = []
@@ -44,7 +27,7 @@ for test_case in range(1, T + 1):
     for i in range(len(org_mat)):
         temp_col = []
         for j in range(len(org_mat)):
-            temp_col.append(org_mat[i][j])
+            temp_col.append(org_mat[j][i])
         raw_col_max.append(temp_col)
         r_raw_dig_max.append(org_mat[i][i])
         l_raw_dig_max.append(org_mat[-i-1][-i-1])
@@ -56,7 +39,7 @@ for test_case in range(1, T + 1):
     dig_max.append(sum(l_raw_dig_max))
     
     max_val_lst = dig_max + col_max + row_max
-    print(max(max_val_lst))
+    print(f'#{test_num} {max(max_val_lst)}')
 
 
     # ///////////////////////////////////////////////////////////////////////////////////
