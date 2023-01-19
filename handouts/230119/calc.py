29816d5beaa3bc9b7bae3f48fc4f43b8e36469b7
seanwoori <seanwoory@gmail.com>
2023-01-19 17:56:46 +0900

    230119 오후 수업 내용 업로드

---
+++ handouts/230119/calc.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        print('0으로 나눌 수 없습니다.')
+    else:
+        return a / b
+
+
