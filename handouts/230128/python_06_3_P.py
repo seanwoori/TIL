69db1c602a83c446d48b7afbbb6fcfe5f7dfba00
seanwoori <seanwoory@gmail.com>
2023-01-28 12:21:43 +0900

    230128 오전 실습 업로드

---
+++ handouts/230128/python_06_3_P.py
@@ -0,0 +1,9 @@
+def count_vowels(str):
+    lst_temp = []
+    for char in str:
+        lst_temp.append(char)
+    print(len(lst_temp))
+
+count_vowels('apple') #=> 2
+count_vowels('banana') #=> 3
+
