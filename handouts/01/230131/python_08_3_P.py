1abf74c9ff8ee6f4a10f250aba0da07b834dcc39
seanwoori <seanwoory@gmail.com>
2023-01-31 17:55:31 +0900

    온실 8_3 풀이 완료

--- handouts/01/230131/python_08_3_P.py
+++ handouts/01/230131/python_08_3_P.py
@@ -1,16 +1,24 @@
 class PublicTransport:
     def __init__(self, name, fare):
         self.name = name
-        self.fare = fare
-        self.pass = 0
+        self.fare = int(fare)
+        self.passenger = 0
 
     def get_in(self, num_in):
+        self.passenger += int(num_in)
+        return self.passenger
         
-    
-    def get_off(self, num_pass):
-
+    def get_off(self, num_off):
+        self.passenger -= int(num_off)
+        return self.passenger
     
     def overall_pass(self):
-
+        self.inpass = input('들어온 승객의 수를 입력하세요: ')
+        self.offpass = input('나간 승객의 수를 입력하세요 : ')
+        return self.get_in(self.inpass) - self.get_off(self.offpass)
     
-    def profit(self, ):
+    def profit(self):
+        print(f'총 수익은 {self.fare * self.overall_pass()}원 입니다.')
+
+pt = PublicTransport('버스', 500)
+pt.profit()
