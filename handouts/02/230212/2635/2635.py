d2bdf563e5847db1a00e054f4eb30187050c6bb4
seanwoori <seanwoory@gmail.com>
2023-02-12 23:28:50 +0900

    230212

---
+++ handouts/02/230212/2635/2635.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# 최대 수?
+n = int(input())
+mx = 0
+ans = []
+
+for i in range(1,n+1)[::-1]:
+    lst = [n]
+    lst.append(i)
+    for j in range(1,30000):
+        if lst[j-1]-lst[j] >= 0:
+            lst.append(lst[j-1]-lst[j])
+        else:
+            break
+
+    if mx < len(lst):
+        mx = len(lst)
+        ans = lst
+
+print(mx)
+print(*ans)
